0fcebed121b207c0f64f445f95cc39f2f0f7a294
n-desantis92 <dsnwebdev@gmail.com>
2021-03-17 15:47:41 +0100

    aggiunto punto 3

--- script/script.js
+++ script/script.js
@@ -20,3 +20,31 @@ console.log(studente);
 for (var key in studente) {
   console.log(studente[key]);
 }
+
+
+// Creare un array di oggetti di studenti.
+var studenti = [
+  studente = {
+    nome: "gino",
+    cognome: "paperino",
+    eta: 33
+  },
+  studente = {
+    nome: "pino",
+    cognome: "alberino",
+    eta: 33
+  },
+  studente = {
+    nome: "pinetta",
+    cognome: "paletta",
+    eta: 33
+  }
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < studenti.length; i++) {
+  console.log(studenti[i]["nome"],studenti[i]["cognome"]);
+}
+
+
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
